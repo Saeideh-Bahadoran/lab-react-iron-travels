382e7fa098bb4e3e45aa20ab4e547266c6bf3ba9
Saeideh Bahadoran <saeideh.bahadoran@gmail.com>
2024-04-03 01:55:06 +0200

    done

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import travelPlansData from "../assets/travel-plans.json";
 
 const TravelList = () => {
-
+    const [list , setList]= useState(travelPlansData)
+function handleDelete(deletingItem){
+    setList(list.filter((currentItem) => currentItem.id != deletingItem.id))
+}
     return (
         <div className="list-container">
-            {travelPlansData.map((currentItem) => {
+            {list.map((currentItem) => {
                 console.log(currentItem)
                 return (
                     <div className="itemContainer" key={currentItem.id}>
@@ -13,12 +17,15 @@ const TravelList = () => {
                             <div className="item-destination">
                                 {currentItem.destination} ({currentItem.days} Days)
                             </div>
-                            <div> {currentItem.description}</div>
+                            <div className="description"> {currentItem.description}</div>
                             <div><span className="price">Price:</span> {currentItem.totalCost} €</div>
                             <div className="labels">
-                                {currentItem.totalCost <= 350 && <div className="label">Great Deal</div>}
+                                {currentItem.totalCost <= 350 && <div className="label greatDeal">Great Deal</div>}
                                 {currentItem.totalCost >= 1500 && <div className="label">Premium</div>}
                                 {currentItem.allInclusive && <div className="label">All Inclusive</div>}
+                            
+                            </div>
+                            <div className="btnContainer"><button className="deleteBtn" onClick={()=>handleDelete(currentItem)}>delete</button>
                             </div>
 
 
